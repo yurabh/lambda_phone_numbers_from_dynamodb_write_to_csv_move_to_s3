ac05c15b4677993e5616440ff76a6172715c39da
yurabh <yurabh1990@gmail.com>
2023-01-16 16:50:21 +0200

    Add dependencies to the lambda function. Created main class function. Created Phone Numbers model.

---
+++ src/main/java/org/example/SnsLambdaHandler.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SnsLambdaHandler {
+    private static final Logger LOGGER = LogManager.getLogger(SnsLambdaHandler.class);
+}
